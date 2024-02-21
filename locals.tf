197034a836ace79beb6d374b3d06d8c4461b1b10
Juan Matias Kungfoo de la Camara Beovide <juan.delacamara@binbash.com.ar>
2024-02-21 11:45:35 -0300

    FIX/ fixed locals logic

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  alarm_topic_arn = var.create_topic == null ? var.existent_topic_arn : aws_sns_topic.canary_alarm[0].arn
+  alarm_topic_arn = var.create_topic == false ? var.existent_topic_arn : aws_sns_topic.canary_alarm[0].arn
 }
